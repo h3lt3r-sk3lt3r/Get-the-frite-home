06b2a68f3fcdd4ebe85f3c428b51e395e41e87b2
Camelia Fauchart <cami.chani1@gmail.com>
2021-11-25 13:37:49 +0100

    finalisation flatpicker

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @stall = Stall.find(params[:stall_id])
+  end
+
+  def create
+    @stall = Stall.find(params[:stall_id])
+    @booking = Booking.new(booking_params)
+    @booking.stall = @stall
+    @booking.user = current_user
+    if @booking.save
+      redirect_to stall_path(@stall)
+    else
+      render :new
+    end
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
