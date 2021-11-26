99c12385e88712b9fd687c25c6bf2a5e991e4e69
Dimitri Bartone <dimitribartone@hotmail.fr>
2021-11-26 15:04:25 +0100

    barre recherche index et declined/accepted ok

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,18 +10,26 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.stall = @stall
     @booking.user = current_user
-
+    @booking.status = "pending"
     if @booking.save
-      redirect_to mystalls_path(@stall)
+      redirect_to mystalls_path
     else
       render :new
     end
   end
 
   def accepted
+    @booking = Booking.find(params[:id])
+    @booking.status = "accepted"
+    @booking.save
+    redirect_to mystalls_path
   end
 
   def declined
+    @booking = Booking.find(params[:id])
+    @booking.status = "declined"
+    @booking.save
+    redirect_to mystalls_path
   end
 
   private
